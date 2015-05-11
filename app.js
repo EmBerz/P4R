522a095a1581b5bbdcb0e751871d5e753a26234c
Emily Berk <Emily_Berk@intuit.com>
2015-05-11 03:47:51 -0700

    Initial COmmit

---
+++ app.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+(function (){
+  'use strict';
+  var Quiz = React.createClass({
+
+    render: function(){
+      return <div>boo</div>;
+    }
+  });
+  React.renderComponent(<Quiz />, document.body);
+})();
