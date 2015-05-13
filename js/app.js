0d43f5cc36a9b2f36d6f6e6da0fb1b118e98b747
Emily Berk <Emily_Berk@intuit.com>
2015-05-12 22:28:13 -0700

    Updating Author Quiz

--- js/app.js
+++ js/app.js
@@ -2,22 +2,53 @@
 (function (){
 /**PLurlsight Course*/
   'use strict';
+  
+  var data = [
+    {name: 'Mark Twain',
+    imageURL: 'images/authors/marktwain.jpg',
+    books: ['The Adventure of Huckleberry Finn']},
+    {name 'Octavia Butler',
+      imageURL: 'images/authors/octaviabutler',
+      books: ['Parable of the Sower', 'Wild Seed', 'Kindred', 'Fledgling']}
+  ]
   var Quiz = React.createClass({
-
+    propTypes: {
+      books: React.PropTypes.array.isRequired
+    },
+    getInitialState: function(){
+      return {
+        author: this.props.data[0],
+        books: this.props.data[0].books
+      };
+    },
     render: function(){
       return <div>
-        {this.props.books.map(function(book){
+        <div className="row">
+          <div className="col-md-4">
+            <img src={this.state.author.imageURL} className="author">
+          </div>
+          <div className="col-md-7">
+        {this.state.books.map(function(book){
           return <Book title={book}/>
-        })}
+        }, this)}            
+          </div>
+          <div className="col-md-1">
+
+          </div>
+        </div>
+
       </div>;
     }
   });
   var Book = React.createClass({
+    propTypes:{
+      title: React.PropTypes.string.isRequired
+    },
     render: function(){
       return <div><h4>{this.props.title}</h4></div>
     }
   })
-  React.render(<Quiz books={["foo", "Parable of the Sower", "Anciliary Sword", "Blindess"]}/>, document.getElementById('quiz'));
+  React.render(<Quiz data={data}/>, document.getElementById('quiz'));
   
 /**Protocol For Relaxation*/
   var Protocol = React.createClass({
@@ -30,6 +61,9 @@
     }
   });
   var Instruction = React.createClass({
+    propTypes: {
+      text: React.PropTypes.string
+    },
     render: function(){
       return <li>
                 <a>@ </a><span>{this.props.text}</span>
